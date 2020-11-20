e79098fe5aadff0bf04bf6f2d7d5bde47ca2bfa4
Asher Hamrick <ashhamrick2@gmail.com>
2020-11-20 16:59:57 -0500

    Query Database

---
+++ src/CSE216DBQuery.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.sql.*;
+
+public class CSE216DBQuery {
+    private static Connection con = CSE216DBConnect.connect();
+
+    public void store(CState obj) {
+        String query = "INSERT INTO objects (hostname, filename, filetype, message) VALUES (" + obj.getHostname() + ", "
+                + obj.getFilename() + ", " + obj.getFiletype() + ", " + obj.getMessage() + ")";
+        try (Statement statement = con.createStatement()) {
+            ResultSet result = statement.executeQuery(query);
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+    }
+
+    public CState retrieve(String user) {
+        String query = "SELECT * FROM objects";
+        try (Statement statement = con.createStatement()) {
+            CState res = new CState();
+            ResultSet result = statement.executeQuery(query);
+            return res;
+        } catch (Exception e) {
+            System.out.println("Error: " + e.getMessage());
+        }
+        return null;
+    }
+}
