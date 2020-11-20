092ce701cadf20b37a0c42702950ca9008b7039a
Asher Hamrick <ashhamrick2@gmail.com>
2020-11-20 17:49:45 -0500

    Fix DBQuery

--- src/CSE216DBQuery.java
+++ src/CSE216DBQuery.java
@@ -5,8 +5,9 @@ public class CSE216DBQuery {
     private static Connection con = CSE216DBConnect.connect();
 
     public void store(CState obj) {
-        String query = "INSERT INTO objects (hostname, filename, filetype, message) VALUES (" + obj.getHostname() + ", "
-                + obj.getFilename() + ", " + obj.getFiletype() + ", " + obj.getMessage() + ")";
+        String query = "INSERT INTO objects (hostname, filename, filetype, message, v, size) VALUES ("
+                + obj.getHostname() + ", " + obj.getFilename() + ", " + obj.getFiletype() + ", " + obj.getMessage()
+                + ", " + obj.getV() + ", " + obj.getFilesize() + ")";
         try (Statement statement = con.createStatement()) {
             ResultSet result = statement.executeQuery(query);
         } catch (Exception e) {
@@ -19,6 +20,13 @@ public class CSE216DBQuery {
         try (Statement statement = con.createStatement()) {
             CState res = new CState();
             ResultSet result = statement.executeQuery(query);
+            result.next();
+            res.setHostname(result.getString("hostname"));
+            res.setFilename(result.getString("filename"));
+            res.setFiletype(result.getString("filetype"));
+            res.setMessage(result.getString("message"));
+            res.setV(result.getInt("v"));
+            res.setFilesize(result.getLong("size"));
             return res;
         } catch (Exception e) {
             System.out.println("Error: " + e.getMessage());
