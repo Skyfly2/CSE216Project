6c99807fa70c9dabc81a2abbeca85270c1b7128d
Asher Hamrick <ashhamrick2@gmail.com>
2020-11-20 13:21:08 -0500

    ClientTransactionLogger changes

--- src/ClientTransactionLogger.java
+++ src/ClientTransactionLogger.java
@@ -1,20 +1,24 @@
 import javax.swing.JFrame;
 import java.net.InetAddress;
 import java.io.File;
+
 /**
  * <p>
- * Title: P2P Project</p>
- *
- * <p>
- * Description: TransactionLogger outputs messages to the
- * Transaction Output Panel.
+ * Title: P2P Project
  * </p>
  *
  * <p>
- * Copyright: none</p>
+ * Description: TransactionLogger outputs messages to the Transaction Output
+ * Panel.
+ * </p>
  *
  * <p>
- * Company: Lehigh University</p>
+ * Copyright: none
+ * </p>
+ *
+ * <p>
+ * Company: Lehigh University
+ * </p>
  *
  * @author Bill Phillips
  *
@@ -24,33 +28,30 @@ public class ClientTransactionLogger {
 
    private String localhost;
    private String filename;
+   private String message;
+   private int count = 0;
 
    public final static int XLOC = 240;
    public final static int YLOC = 350;
    public final static int WIDTH = 600;
    public final static int HEIGHT = 200;
 
-   private boolean isguiinterface
-        = RunTimeVars.Instance().isGUIInterface();
+   private boolean isguiinterface = RunTimeVars.Instance().isGUIInterface();
 
-   private static TransactionOutputPanel  topanel;
-   private static ClientTransactionLogger 
-                                  ctolgger = null;
+   private static TransactionOutputPanel topanel;
+   private static ClientTransactionLogger ctolgger = null;
 
-    //
+   //
    // The Singleton Design Pattern.
    //
-   public static ClientTransactionLogger Instance() 
-   {
-      if (ctolgger == null) 
-      {
+   public static ClientTransactionLogger Instance() {
+      if (ctolgger == null) {
          ctolgger = new ClientTransactionLogger();
       }
       return ctolgger;
    }
 
-   public void writeToLogger(final String S) 
-   {
+   public void writeToLogger(final String S) {
       if (isguiinterface) {
          topanel.setText(S);
          topanel.repaint();
@@ -58,44 +59,43 @@ public class ClientTransactionLogger {
       SimpleLog.write(filename, S);
    }
 
-   public void writeToLogger
-                     (final String S, final int i) 
-   {
+   public void writeToLogger(final String S, final int i) {
       writeToLogger(String.format("%s %d", S, i));
+   }
+
+   public void logClient(String message) {
+      this.message = message;
+      writeToLogger(message);
+   }
+
+   public int countClient() {
+      this.count = this.count + 1;
+      return this.count;
    }
 
    private ClientTransactionLogger() {
       try {
-         localhost = InetAddress.
-                     getLocalHost().getHostName();
-      } 
-      catch (Exception e) 
-      {
+         localhost = InetAddress.getLocalHost().getHostName();
+      } catch (Exception e) {
+         System.out.println("Connection failed");
       }
 
       if (isguiinterface) {
-         topanel = new TransactionOutputPanel(
-                "Client Transactions for host: " + 
-                               localhost, 20, 80);
-         topanel.setDefaultCloseOperation
-                     (JFrame.DO_NOTHING_ON_CLOSE);
+         topanel = new TransactionOutputPanel("Client Transactions for host: " + localhost, 20, 80);
+         topanel.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
          topanel.setSize(WIDTH, HEIGHT);
          topanel.setLocation(XLOC, YLOC);
          topanel.setResizable(true);
          topanel.setVisible(true);
       }
 
-        //
+      //
       // Create the log file name.
       // If a log file exists, delete it.
       //
-      filename = "ClientOutputLog"
-              + "-From-"
-              + localhost
-              + ".txt";
+      filename = "ClientOutputLog" + "-From-" + localhost + ".txt";
       final File f = new File(filename);
-      if (f.exists()) 
-      {
+      if (f.exists()) {
          f.delete();
       }
 
